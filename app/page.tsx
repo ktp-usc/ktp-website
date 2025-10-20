2b1f231adad2ee2b848885e08cbff59d6086b70b
wynter <wynters@email.sc.edu>
2025-10-20 19:45:06 -0400

    combining website features

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,17 @@ import Image from "next/image";
 import { Header } from "./Header";
 import Footer from "./Footer";
 import logo from '../SquareLogoGarnet.png';
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious,} from "@/components/ui/carousel"
+
+const data = [
+    { id: 1, title: "SC Economics", text: "The South Carolina Council on Economic Education promotes financial and economic literacy for students and teachers across the state. Our members are collaborating directly with the organization’s executive leadership to design and implement a digital solution that streamlines operations and strengthens the impact of economic education throughout South Carolina.",
+        image: "/partners/scecon.png"},
+    { id: 2, title: "Ella's Stuff A Stocking", text: "Ella’s Stuff a Stocking is a holiday donation drive established in memory of Gabriella Shumate, dedicated to spreading joy by providing gifts and essentials to those in need during the holiday season. Our team is rebuilding their site from the ground up, creating a reliable, user-friendly platform that enhances community engagement and ensures the continued success of their annual giving campaign.",
+        image: "/partners/ellas.png"},
+    { id: 3, title: "Wheels Harbison Area Transit", text: "Wheels Harbison Area Transit is a local nonprofit that provides essential mobility services to low-income seniors and disabled residents in the Harbison area. Our team is developing a professional website and digital presence for Wheels Harbison, enabling them to share their mission, accept online donations, and attract corporate sponsorships to sustain their operations.",
+        image: "/partners/wheels.png"},
+];
 
 export default function Home() {
     return (
