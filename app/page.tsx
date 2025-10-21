f2f91006a7863e6067eb3993a490a7d3c2ba033f
WynterS04 <143433190+WynterS04@users.noreply.github.com>
2025-10-20 22:16:26 -0400

    feature/KTP-7-Combine-website-features (#5)
    * added images and structure.
    
    * Committing a header and footer added to the website
    
    * combining website features
    
    * combining website features
    
    * combining website features
    
    * combining website features
    
    * adding pictures
    
    ---------
    
    Co-authored-by: gwen <gwendalynkrauss@gmail.com>
    Co-authored-by: Marvin Thomas <marvinthomas200517@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,240 @@
 import Image from "next/image";
+import { Header } from "./Header";
+import Footer from "./Footer";
+import logo from '../SquareLogoGarnet.png';
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious,} from "@/components/ui/carousel"
+
+const data = [
+    { id: 1, title: "SC Economics", text: "The South Carolina Council on Economic Education promotes financial and economic literacy for students and teachers across the state. Our members are collaborating directly with the organization’s executive leadership to design and implement a digital solution that streamlines operations and strengthens the impact of economic education throughout South Carolina.",
+        image: "/partners/scecon.png"},
+    { id: 2, title: "Ella's Stuff A Stocking", text: "Ella’s Stuff a Stocking is a holiday donation drive established in memory of Gabriella Shumate, dedicated to spreading joy by providing gifts and essentials to those in need during the holiday season. Our team is rebuilding their site from the ground up, creating a reliable, user-friendly platform that enhances community engagement and ensures the continued success of their annual giving campaign.",
+        image: "/partners/ellas.png"},
+    { id: 3, title: "Wheels Harbison Area Transit", text: "Wheels Harbison Area Transit is a local nonprofit that provides essential mobility services to low-income seniors and disabled residents in the Harbison area. Our team is developing a professional website and digital presence for Wheels Harbison, enabling them to share their mission, accept online donations, and attract corporate sponsorships to sustain their operations.",
+        image: "/partners/wheels.png"},
+];
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
+    return (
+      <div className="font-sans min-h-screen p-8 pb-20 flex flex-col items-center">
+        <main className="flex flex-col items-center w-full">
+          <Header />
+          <div className="border-4 border-white rounded-xl mt-12 p-6 shadow-sm text-center max-w-3xl mx-auto">
+            <div className="relative w-48 h-48 mx-auto mb-6">
+              <Image src={logo} alt="Logo" fill style={{ objectFit: 'contain' }} />
+            </div>
+            <div className="border-2 border-gray-400 rounded-xl p-6 text-center shadow-md mt-10 max-w-3xl mx-auto bg-white/5 backdrop-blur-sm">
+              <h2 className="text-2xl font-semibold text-left mb-4">About Our Chapter</h2>
+              <p className="text-lg leading-relaxed text-gray-700">
+                Kappa Theta Pi at the University of South Carolina is committed to
+                fostering a community of passionate technologists, innovators, and
+                leaders. We host workshops, networking events, and professional
+                development sessions that empower members to grow both personally and
+                professionally.
+              </p>
+            </div>
+          </div>
+            <div className="max-w-3xl w-full mx-auto">
+                <h2 className="text-3xl font-bold text-left">
+                    Ongoing Projects
+                </h2>
+                <div className="relative">
+                    <Carousel className="w-full border-2 border-gray-400 rounded-xl p-6 shadow-md mt-4">
+                        <CarouselContent>
+                            {data.map((p)=>(
+                                <CarouselItem key={p.id} className="w-full">
+                                    <div className="flex flex-col md:flex-row items-start">
+
+                                        <div className="w-full md:w-1/2 p-2" >
+                                            <h2 className="text-xl font-semibold mb-2 text-gray-900">
+                                                {p.title}
+                                            </h2>
+                                            <p className="leading-relaxed text-gray-700">
+                                                {p.text}
+                                            </p>
+                                        </div>
+
+                                        <div className="relative w-full md:w-1/2 h-64 md:h-auto p-4 flex items-center justify-center">
+                                            <div className="relative w-full h-full overflow-hidden rounded-xl">
+                                                <AspectRatio ratio={5 / 4}>
+                                                    <Image src={p.image} alt={"${project} logo"} fill className="object-cover"/>;
+                                                </AspectRatio>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <CarouselPrevious aria-label="Previous" />
+                        <CarouselNext aria-label="Next"/>
+                    </Carousel>
+                </div>
+            </div>
+
+          <div className="w-full flex flex-col items-center">
+            <h2 className="text-3xl font-bold text-center mt-12 mb-8">Meet the Executive Board</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 justify-items-center">
+
+
+          <div className="flex flex-col items-center">
+          <Image src="/Images/Screenshot Owen.png"  
           alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                    <a
+            className="mt-4 rounded bg-[#143d76] px-4 py-2 text-white cursor-pointer hover:bg-[#19FF19]"
+            href="https://www.linkedin.com/in/owencoulam/"
             target="_blank"
             rel="noopener noreferrer"
+            
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+            
+            President: <span>Owen Coulam</span>
           </a>
         </div>
+        <div className="flex flex-col items-center">
+                  <Image
+          src="/Images/Screenshot Darssan.png"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+
+                              <a
+            className="mt-4 rounded bg-[#143d76] px-4 py-2 text-white cursor-pointer hover:bg-[#19FF19]"
+            href="https://www.linkedin.com/in/ledarssan/"
+            target="_blank"
+            rel="noopener noreferrer"
+            
+          >
+            Vice President: <span>Darssan Eswaramoorthi</span>
+          </a>
+          </div>
+        <div className="flex flex-col items-center">
+          <Image
+          src="/Images/Screenshot Josiah.png"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+                              <a
+            className="mt-4 rounded bg-[#143d76] px-4 py-2 text-white cursor-pointer hover:bg-[#19FF19]"
+            href="https://www.linkedin.com/in/josiahawhite/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Executive Secretary: <span>Josiah White</span>
+          </a>
+          </div>
+          <div className="flex flex-col items-center">
+          <Image
+          src="/Images/Screenshot Luke.png"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+                              <a
+            className="mt-4 rounded bg-[#143d76] px-4 py-2 text-white cursor-pointer hover:bg-[#19FF19]"
+            href="https://www.linkedin.com/in/lukejannazzo/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Director of Outreach: <span>Luke Jannazzo</span>
+          </a>
+          </div>
+          <div className="flex flex-col items-center">
+                  <Image
+          src="/Images/Screenshot Braden.png"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+                              <a
+            className="mt-4 rounded bg-[#143d76] px-4 py-2 text-white cursor-pointer hover:bg-[#19FF19]"
+            href="https://www.linkedin.com/in/bguliano/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Director of Technical Development: <span>Braden Guliano</span>
+          </a>
+          </div>
+          <div className="flex flex-col items-center">
+                  <Image
+          src="/Images/Screenshot Sara.png"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+           <a
+            className="mt-4 rounded bg-[#143d76] px-4 py-2 text-white cursor-pointer hover:bg-[#19FF19]"
+            href="https://www.linkedin.com/in/sara-muthu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Director of Finance: <span>Sara Muthuselvam</span>
+          </a>  
+          </div>
+          <div className="flex flex-col items-center">
+         <Image
+          src="/Images/Screenshot Katie.png"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />                         <a
+            className="mt-4 rounded bg-[#143d76] px-4 py-2 text-white cursor-pointer hover:bg-[#19FF19]"
+            href="https://www.linkedin.com/in/katiejones404/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Director of Marketing: <span>Katie Jones</span>
+          </a>    
+          </div>
+          <div className="flex flex-col items-center">
+         <Image
+          src="/Images/Screenshot Sai.png"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />                         
+         <a
+            className="mt-4 rounded bg-[#143d76] px-4 py-2 text-white cursor-pointer hover:bg-[#19FF19]"
+            href="https://www.linkedin.com/in/sai-kottapali-153695288/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Director of Engagement: <span>Sai Kottapali</span>
+          </a>    
+          </div>
+          <div className="flex flex-col items-center">
+          <Image
+          src="/Images/Screenshot Tarun.png"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />                          
+        <a
+            className="mt-4 rounded bg-[#143d76] px-4 py-2 text-white cursor-pointer hover:bg-[#19FF19]"
+            href="https://www.linkedin.com/in/tarun-ramkumar/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Director of Professional Development: <span>Tarun Ramkumar</span>
+          </a>
+          </div>
+          </div>
+          </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
