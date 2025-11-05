44ea00e386e8e9cffce8f078f004c5e66a21819e
gwen <gwendalynkrauss@gmail.com>
2025-11-05 17:12:12 -0500

    KTP-17

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,7 +3,7 @@ import logo from '../CircleLogo-Transparent.png';
                           
 export function Header() {
   return (
-    <header className="w-full bg-white shadow-md fixed top-0 left-0 rounded-2xl">
+    <header className="fixed top-0 left-0 right-0 bg-white shadow-md z-50">
       <div className="max-w-5xl mx-auto px-6 py-4">
         <Image src={logo} alt="Logo" width={40} height={40} className="inline-block mr-3" />
       </div>
