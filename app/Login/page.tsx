9aa0849ef61f79a5c0bdf6d3ad838c99fac738a3
Brett-Eck <bertteckstrom@gmail.com>
2025-12-19 18:41:51 -0500

    Added login button and linked it to an empty login page

---
+++ app/Login/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function LoginPage() {
+    
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-center mb-5">Login Page</h1>
+        </div>
+    );
+}
