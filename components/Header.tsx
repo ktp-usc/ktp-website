a588c413a68000b5490a6c80ce59db6780c4aed8
neyamCSE <neyam@email.sc.edu>
2026-01-13 11:26:09 -0500

    KTP-58 Create our work page (#35)
    * Update application/page.tsx to KTP's current application template
    
    * Update Hometown and Major sections of application
    
    * Enhance application page introduction with additional context and updated styling
    
    * Validate USC email domain and restrict resume uploads to PDF only
    
    * Further changes to application formatting
    
    * Deleted note that saying college freshmen should put "N/A" for GPA
    
    * Fixed JSX unescaped entities causing build failure
    
    * Added application deadline card
    
    * Enhance application page with required resume and photo uploads
    
    * added cursor-pointer to buttons
    
    * Creating the our work page and adding nonprofit projects
    
    * altered order of clients
    
    * installed packages
    
    * fixed file paths
    
    * ran npm run build
    
    * KTP-58 update package-lock.json
    
    * KTP-58 fix `npm run build` errors
    
    * KTP-58 fix `npm run build` dynamic/static build errors
    
    * Adding images from symposium night
    
    * KTP-58 fixed broken link in Footer; fixed `npm run build` errors
    
    ---------
    
    Co-authored-by: sebastianboscan <sebastianboscan@hotmail.fr>
    Co-authored-by: ocoulam <owen.coulam.1@gmail.com>
    Co-authored-by: ChristianGWolff45 <Christianwolff@gmail.com>
    Co-authored-by: Braden Guliano <72895782+bguliano@users.noreply.github.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,7 +25,7 @@ export function Header() {
     }, []);
 
     return (
-        <header className={`sticky top-0 w-full z-[9999] transition-all duration-700 p-2 sm:p-4 ${isScrolled ? 'bg-white shadow-md' : 'bg-transparent'}`}>
+        <header className={`sticky top-0 w-full z-9999 transition-all duration-700 p-2 sm:p-4 ${isScrolled ? 'bg-white shadow-md' : 'bg-transparent'}`}>
             <div className="relative flex items-center justify-center w-full px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 py-2 sm:py-3 z-10">
 
                 {/* Logo - Scales from w-8 (mobile) back to your original w-11 (desktop) */}
@@ -42,10 +42,13 @@ export function Header() {
                 {/*NavBar*/}
                 <nav className='flex flex-wrap justify-center text-xs sm:text-md md:text-lg space-x-4 sm:space-x-8 md:space-x-12 lg:space-x-16'>
                     {/*<Link className="hover:text-[#315CA9] font-medium" href="/">Home</Link>*/}
-                    <Link className="hover:text-[#315CA9] font-medium transition-colors" href="/members">Members</Link>
-                    <Link className="hover:text-[#315CA9] font-medium transition-colors" href="/rush">Rush</Link>
-                    <Link className="hover:text-[#315CA9] font-medium transition-colors" href="/apply">Apply</Link>
+
+                    <Link className="hover:text-[#315CA9] font-medium" href="/members">Members</Link>
+                    <Link className="hover:text-[#315CA9] font-medium" href="/rush">Rush</Link>
+                    <Link className="hover:text-[#315CA9] font-medium" href="/clients">Our Work</Link>
+                    <Link className="hover:text-[#315CA9] font-medium" href="/application">Apply</Link>
                 </nav>
+
                 <div className="absolute right-1 sm:right-6 md:right-12 lg:right-16 xl:right-20">
                     <Link
                         className="bg-[#315CA9] text-white font-medium px-3 py-1.5 sm:px-4 sm:py-2 rounded-md text-xs sm:text-base hover:bg-[#23498F] transition-all duration-300 transform shadow-lg hover:drop-shadow-lg"
