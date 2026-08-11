aa158d46000a81f7d01b63accae2047e57699ca5
Aden Harrington <reddragon0828@gmail.com>
2026-08-11 13:25:02 -0400

    Calendar Page for KTP Website.

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,6 +19,7 @@ const NAV_LINKS = [
     { href: "/", label: "Home" },
     { href: "/members", label: "Members" },
     { href: "/clients", label: "Our Work" },
+    { href: "/calendar", label: "Calendar" },
     { href: "/donate", label: "Donate" },
     { href: "/rush", label: "Rush" },
     { href: "/apply", label: "Apply" }
